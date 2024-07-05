75ba695354ffc4e77f2861d6613d34cf99cfbe79
farhanjums <83050550+farhanjums@users.noreply.github.com>
2024-07-06 03:43:31 +0800

    Update appRoute.js

--- appServer1/routes/appRoute.js
+++ appServer1/routes/appRoute.js
@@ -27,7 +27,7 @@ router.post('/submit',upload.fields([]),(req, res) => {
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.location('http://localhost:5001/api/feedback')
       console.log(req.body);
-      var query1="INSERT INTO `ict2509c` (`ques1`, `ques2`, `ques3`, `name`) VALUES ('"+req.body.Q1+"','"+req.body.Q2+"','"+ req.body.Q3+"','"+ req.body.text+"')";
+      var query1="INSERT INTO ict2509c (ques1, ques2, ques3, name) VALUES ('"+req.body.Q1+"','"+req.body.Q2+"','"+ req.body.Q3+"','"+ req.body.text+"')";
       console.log(query1);
       con.query(query1,(err,result)=>{
         console.log(result);
