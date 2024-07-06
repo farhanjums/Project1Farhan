a59e6f10d0e9d9649edd01f0ef647b33e14c2c02
farhanjums <83050550+farhanjums@users.noreply.github.com>
2024-07-06 10:02:22 +0800

    Add files via upload

---
+++ appServer1/views/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'farhan-database-1.cwkgdwd2duoh.us-east-1.rds.amazonaws.com',
+    DB_USER : 'root',
+    DB_PWD : 'root',
+    DB_DATABASE : 'project1'
+});
